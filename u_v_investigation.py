f4a6fddd913e52342ca2eb67af4e0d456e6be490
19michaelaap <polleym@carleton.edu>
2020-04-19 20:10:09 -0500

    Work on looking at u's and v;s

---
+++ u_v_investigation.py
@@ -0,0 +1,41 @@
+import math
+#looking just at u = -v
+# for n in range (1, 2000):
+#     m = math.sqrt(1296 + n**2)
+#     mInt = int(m)
+#     if m == mInt:
+#         print(m, "      ", math.sqrt(36+m), "     ")
+
+#still just looking at u = -v
+# for u in range (9, 2000):
+#     d = math.sqrt(u**2*(-72+u**2))
+#     dInt = int(d)
+#     if (d==dInt):
+#         print(u)
+
+#Looking more broadly at u and v
+for u  in range (-10000,10000):
+    if u != 0: #u can't be zero, otherwise many entries are undefined
+        for v in range (-10000,10000):
+            if v/u == v//u: # checking that a12 and a33 are integers
+                entry32 = (-(u**2)+u*v-(v**2))/(u**2)
+                entry32Int = int(entry32) 
+                if entry32 == entry32Int: #checking that a32 is an integer
+                    t = 2*(u**2+v**2)*(u**2-u*v+v**2) #computing the value to tau
+                    d = (-4)*(u**2)*(5+u)+(4)*(u)*(8+u)*(v)+(-20+(u)*(4+u))*(v**2)-4*(v**3) # computing the value of d
+                    if d > 0: #d has to be positive for its square root to be an integer
+                        dRoot = math.sqrt(d)
+                        entry13 = u*(-*(u**3)*v+4*u*(v**2)-2*(v**3)+(u**2)*(-6*v+dRoot))/t
+                        entry13Int = int(entry13)
+                        if entry13 == entry13Int: #checking that a13 is an integer
+                            #at this point A is in SL3Z since a matrix of this form will always have det 1
+                            b13 = u * (-2*(u**3)+2*(v**3)-(u**2)*v*(2+v)+u*v*(-2*v + dRoot))/t
+                            b13Int = int(b13)
+                            if b13 == b13Int: #checking that b13 is an integer
+                                b21 = (-2*(v**2) + (u**2)*(2+v) + u *(4*v+ dRoot))/ (2* (u**2))
+                                b21Int = int(b21)
+                                if b21Int == b21: #checking that b21 is an integer
+                                    b31 = (-2*(u**3)-2*(v**3)+(u**2)*v*(2+v)+u*v*(2*v+dRoot))/(2*(u**3))
+                                    b31Int = int(b31)
+                                    if b31 == b31Int: #checking that b31 is an integer
+                                        print("Solution: ",u,v)
