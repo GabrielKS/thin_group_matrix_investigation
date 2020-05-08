bce5cb01c16b36d07fb66dad4c653dbda823d63c
gabrielks <23368820+GabrielKS@users.noreply.github.com>
2020-05-08 18:27:22 -0500

    Add notepad.py for miscellaneous small experiments

--- notepad.py
+++ notepad.py
@@ -2,22 +2,38 @@ from common import *
 from conversion import *
 import numpy as np
 
-full_str = "BBABB"
-h1_str = "BBABBAABAA"
-h2_str = "BBABBABBAB"
-full_ref = 100  # 1938285, 1 1101100100 1101101101
-h1_ref = 1892  # 1 1101100100
-h2_ref = 1901  # 1 1101101101
-str_length = ref_to_length(full_ref)
-cache_length = 10
-n = min(cache_length, str_length)
+# full_str = "BBABB"
+# h1_str = "BBABBAABAA"
+# h2_str = "BBABBABBAB"
+# full_ref = 100  # 1938285, 1 1101100100 1101101101
+# h1_ref = 1892  # 1 1101100100
+# h2_ref = 1901  # 1 1101101101
+# str_length = ref_to_length(full_ref)
+# cache_length = 10
+# n = min(cache_length, str_length)
 
-# print(int_to_h(full_ref))
-# print(max(full_ref >> cache_length, 1))
-# print((full_ref & ((1 << n)-1) | (1 << n)))
+# # print(int_to_h(full_ref))
+# # print(max(full_ref >> cache_length, 1))
+# # print((full_ref & ((1 << n)-1) | (1 << n)))
+# # print(ref_last_n(full_ref, cache_length))
+# print(full_ref)
+# print(bin(full_ref))
+# print(bin(ref_last_n(full_ref, cache_length)))
+# print(ref_first_n(full_ref, cache_length))
 # print(ref_last_n(full_ref, cache_length))
-print(full_ref)
-print(bin(full_ref))
-print(bin(ref_last_n(full_ref, cache_length)))
-print(ref_first_n(full_ref, cache_length))
-print(ref_last_n(full_ref, cache_length))
+
+# print(np.argmax([False, True]))
+
+# for crr in (19173961, 26364196, 38347922, 52728393, 76695844, 105456787):
+#     print(crr)
+#     print(ref_to_length(crr))
+#     print(int_to_h(crr))
+#     print(h_to_mat(int_to_h(crr)))
+#     print()
+
+# X = h_to_mat("BAABAABAABAABAABAABAABAA")
+# print(A*A*X)
+# print(X*B*B)
+# print(B*A*A)
+
+print(ref_to_length(1947355))
